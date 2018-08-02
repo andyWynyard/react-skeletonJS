26f74a70db58928580b04686d0273f48a9f06aa9
Andrew Wynyard <andrew.wynyard@gmail.com>
2018-08-02 21:55:50 +0200

    fixed imports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,7 @@
-import Heading1 from './components/Heading1';
+import Heading1 from './components/atoms/Heading1';
+import Heading2 from './components/atoms/Heading2';
 import Button from './components/atoms/Button';
+import Input from './components/atoms/Input';
+import Link from './components/atoms/Link';
 
-export { Heading1, Button };
+export { Heading1, Heading2, Button, Input, Link };

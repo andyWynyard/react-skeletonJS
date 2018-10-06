6d6f9dc0aaaf540875ae908eac804339be0afeae
andyWynyard <andrew.wynyard@gmail.com>
2018-10-06 15:51:53 +0200

    Updated storybook to reflect version

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,19 @@
-import Heading1 from './components/atoms/Heading1';
-import Heading2 from './components/atoms/Heading2';
-import Button from './components/atoms/Button';
-import Input from './components/atoms/Input';
-import Link from './components/atoms/Link';
-import BoxedHeading from './components/molecules/BoxedHeading';
+import Heading1 from "./components/atoms/Heading1";
+import Heading2 from "./components/atoms/Heading2";
+import Heading3 from "./components/atoms/Heading3";
+import Heading4 from "./components/atoms/Heading4";
+import Button from "./components/atoms/Button";
+import Input from "./components/atoms/Input";
+import Link from "./components/atoms/Link";
+import BoxedHeading from "./components/molecules/BoxedHeading";
 
-export { Heading1, Heading2, Button, Input, Link, BoxedHeading };
+export {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Button,
+  Input,
+  Link,
+  BoxedHeading
+};
